aa1e7473e3e8c17059e56ae7879c728e7c3192e8
Baba Dorin <bvd.dorin@gmail.com>
2020-12-29 00:56:14 +0200

    [repository-and-database-init] IUnitOfWork Added

---
+++ API/temsAPI/temsAPI/Contracts/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using temsAPI.Data.Entities;
+
+namespace temsAPI.Contracts
+{
+    interface IUnitOfWork: IDisposable
+    {
+        IGenericRepository<Equipment> Equipment { get; }
+        IGenericRepository<EquipmentDefinition> EquipmentDefinitions { get; }
+        IGenericRepository<EquipmentSpecifications> EquipmentSpecifications { get; }
+        IGenericRepository<EquipmentType> EquipmentTypes { get; }
+        IGenericRepository<Property> Properties { get; }
+        IGenericRepository<DataType> DataType { get; }
+        IGenericRepository<PropertyEquipmentTypeAssociation> PropertyEquipmentTypeAssociations { get; }
+
+        Task Save();
+    }
+}

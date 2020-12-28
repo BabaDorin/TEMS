9968cb48b7c5b39d6bab88b9152138192f7b8acc
Baba Dorin <bvd.dorin@gmail.com>
2020-12-29 00:37:47 +0200

    Core models - Added

---
+++ API/temsAPI/temsAPI/Data/Entities/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace temsAPI.Data.Entities
+{
+    public class Property
+    {
+        [Key]
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+        
+        [ForeignKey("DataTypeID")]
+        public DataType DataType { get; set; }
+        public string DataTypeID { get; set; }
+    }
+}
---
+++ API/temsAPI/temsAPI/Data/Entities/PropertyEquipmentTypeAssociation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace temsAPI.Data.Entities
+{
+    public class PropertyEquipmentTypeAssociation
+    {
+        [ForeignKey("TypeID")]
+        public EquipmentType Type { get; set; }
+        public string TypeID { get; set; }
+
+        [ForeignKey("PropertyID")]
+        public Property Property { get; set; }
+        public string PropertyID { get; set; }
+    }
+}

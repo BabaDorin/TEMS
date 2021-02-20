b4bd923674f1f3703db0d1a7f3734b0c20f8bcc2
Baba Dorin <bvd.dorin@gmail.com>
2021-02-20 16:40:16 +0200

    [ui-dev] Add Log from EquipmentDetails Component -> implemented

--- UI/temsUI/src/app/tems-components/equipment/equipment-details/equipment-details-logs/equipment-details-logs.component.ts
+++ UI/temsUI/src/app/tems-components/equipment/equipment-details/equipment-details-logs/equipment-details-logs.component.ts
@@ -2,6 +2,8 @@ import { EquipmentService } from './../../../../services/equipment-service/equip
 import { Component, Input, OnInit } from '@angular/core';
 import { LogsService } from 'src/app/services/logs-service/logs.service';
 import { ViewLog } from 'src/app/models/communication/logs/view-logs.model';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AddLogComponent } from 'src/app/tems-components/communication/add-log/add-log.component';
 
 @Component({
   selector: 'app-equipment-details-logs',
@@ -13,11 +15,28 @@ export class EquipmentDetailsLogsComponent implements OnInit {
   @Input() equipmentId;
   logs: ViewLog[];
 
-  constructor(private logsService: LogsService) { 
-
+  constructor(
+    private logsService: LogsService,
+    public dialog: MatDialog) { 
   }
+
+
 
   ngOnInit(): void {
     this.logs = this.logsService.getLogsByEquipmentId(this.equipmentId);
+  }
+
+  addLog(){
+    this.openAddLogDialog();
+  }
+  
+  openAddLogDialog(): void {
+    let dialogRef: MatDialogRef<any>;
+    dialogRef = this.dialog.open(AddLogComponent); 
+    dialogRef.componentInstance.equipment = {id: this.equipmentId, value: '1'};
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 }

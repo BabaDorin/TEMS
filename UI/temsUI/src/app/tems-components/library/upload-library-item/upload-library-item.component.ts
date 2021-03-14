215bd8081eea5c7a1c8278a55735c63b5dd89ed7
Baba Dorin <bvd.dorin@gmail.com>
2021-03-14 13:53:26 +0200

    [fe-be-interaction] Now the user can remove files from queue event if the upload started

--- UI/temsUI/src/app/tems-components/library/upload-library-item/upload-library-item.component.ts
+++ UI/temsUI/src/app/tems-components/library/upload-library-item/upload-library-item.component.ts
@@ -27,58 +27,58 @@ export class UploadLibraryItemComponent extends TEMSComponent implements OnInit 
   private formDatas: FormData[] = [];
 
   upload() {
-    // console.log(this.files[0]);
-    // console.log(this.files[1]);
     if (this.files.length === 0)
       return;
 
     this.uploadFile(0);
-
-    // for (let index = 0; index < this.files.length; index++) {
-    //   if(this.files[index] == null)
-    //     continue;
-    // }
   }
 
   uploadFile(index) {
+    console.log(index);
     // If current null - pick next
     if(this.files[index] == null)
-      if(index < this.files.length)
-        this.uploadFile(++index);
+      if(++index < this.files.length)
+        return this.uploadFile(index);
       else
         return;
 
     let fileToUpload = this.files[index];
+    fileToUpload.status = "uploading";
 
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
     formData.append('myName', fileToUpload.myName)
     formData.append('myDescription', fileToUpload.myDescription)
-    this.formDatas.push(formData);
-
+    this.formDatas[index]=formData;
+    
+    console.log(this.formDatas[index]);
     this.subscriptions.push(this.libraryService.uploadFile(this.formDatas[index])
       .subscribe(event => {
-        console.log(event);
+        // console.log(event);
 
         if (event.type === HttpEventType.UploadProgress)
           this.files[index].progress = Math.round(100 * event.loaded / event.total);
         else if (event.type === HttpEventType.Response) {
           this.files[index].message = 'Upload success.';
-  
-          if(index < this.files.length)
-            this.uploadFile(++index);
+
+          fileToUpload.status = "uploaded";
+          if(++index < this.files.length)
+            return this.uploadFile(index);
         }
       }));
   }
 
   //  Convert Files list to normal array list
   prepareFilesList(files: Array<any>) {
+    this.files = [];
+    this.formDatas = [];
     for (const item of files) {
       item.progress = 0;
       item.myName = '';
       item.myDescription = '';
       item.message = '';
       this.files.push(item);
+      this.formDatas.push(new FormData());
     }
   }
 
@@ -91,7 +91,7 @@ export class UploadLibraryItemComponent extends TEMSComponent implements OnInit 
   }
 
   deleteFile(index: number) {
-    this.libraryService.cancelThread(0).subscribe(result => console.log(result));
+    // this.libraryService.cancelThread(0).subscribe(result => console.log(result));
     this.formDatas[index] = null;
     this.files[index] = null;
   }

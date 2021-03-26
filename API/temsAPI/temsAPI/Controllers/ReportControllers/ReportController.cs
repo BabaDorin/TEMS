e28235313b97632fcca9b815e204a8b887b48ea3
Baba Dorin <bvd.dorin@gmail.com>
2021-03-26 06:59:58 +0200

    [fe-be-interaction] Saving report templates

--- API/temsAPI/temsAPI/Controllers/ReportControllers/ReportController.cs
+++ API/temsAPI/temsAPI/Controllers/ReportControllers/ReportController.cs
@@ -1,14 +1,19 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using temsAPI.Contracts;
+using temsAPI.Data.Entities.EquipmentEntities;
+using temsAPI.Data.Entities.OtherEntities;
+using temsAPI.Data.Entities.Report;
 using temsAPI.Data.Entities.UserEntities;
 using temsAPI.System_Files;
+using temsAPI.ViewModels;
 using temsAPI.ViewModels.Report;
 
 namespace temsAPI.Controllers.ReportControllers
@@ -19,20 +24,83 @@ namespace temsAPI.Controllers.ReportControllers
         {
         }
 
-        //[HttpPost]
-        //[ClaimRequirement(TEMSClaims.CAN_MANAGE_ENTITIES)]
-        //public async Task<JsonResult> AddTemplate([FromBody] AddReportTemplateViewModel viewModel)
-        //{
-        //    try
-        //    {
+        [HttpPost]
+        [ClaimRequirement(TEMSClaims.CAN_MANAGE_ENTITIES)]
+        public async Task<JsonResult> AddTemplate([FromBody] AddReportTemplateViewModel viewModel)
+        {
+            try
+            {
+                string validationMessage = await ValidateTemplate(viewModel);
+                if (validationMessage != null)
+                    return ReturnResponse(validationMessage, ResponseStatus.Fail);
 
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Debug.WriteLine(ex);
-        //        return ReturnResponse("An error occured while saving the template", ResponseStatus.Fail);
-        //    }
-        //}
+                List<string> typeIds = viewModel.Types?.Select(q => q.Value).ToList();
+                List<string> definitionIds = viewModel.Definitions?.Select(q => q.Value).ToList();
+                List<string> roomIds = viewModel.Rooms?.Select(q => q.Value).ToList();
+                List<string> personnelIds = viewModel.Personnel?.Select(q => q.Value).ToList();
+                List<string> specificProperties = viewModel.SpecificProperties?.SelectMany(q => q.Properties).ToList();
+                List<string> propertyIds = viewModel.CommonProperties?
+                    .Concat(specificProperties == null ? new List<string>() : specificProperties)
+                    .ToList();
+                List<string> signatoriesIds = viewModel.Signatories?.Select(q => q.Value).ToList();
+
+                ReportTemplate model = new ReportTemplate
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = viewModel.Name,
+                    Description = viewModel.Description,
+                    Subject = viewModel.Subject,
+                    EquipmentTypes = (typeIds != null)
+                        ? (await _unitOfWork.EquipmentTypes
+                        .FindAll<EquipmentType>(q => typeIds.Contains(q.Id)))
+                        .ToList()
+                        : new List<EquipmentType>(),
+                    EquipmentDefinitions = (definitionIds != null)
+                        ? (await _unitOfWork.EquipmentDefinitions
+                        .FindAll<EquipmentDefinition>(q => definitionIds.Contains(q.Id)))
+                        .ToList()
+                        : new List<EquipmentDefinition>(),
+                    Rooms = (roomIds != null)
+                        ? (await _unitOfWork.Rooms
+                        .FindAll<Room>(q => roomIds.Contains(q.Id)))
+                        .ToList()
+                        : new List<Room>(),
+                    Personnel = (personnelIds != null)
+                        ? (await _unitOfWork.Personnel
+                        .FindAll<Personnel>(q => personnelIds.Contains(q.Id)))
+                        .ToList()
+                        : new List<Personnel>(),
+                    SepparateBy = viewModel.SepparateBy,
+                    Properties = (propertyIds != null)
+                        ? (await _unitOfWork.Properties
+                        .FindAll<Property>(q => propertyIds.Contains(q.Id)))
+                        .ToList()
+                        : new List<Property>(),
+                    Header = viewModel.Header,
+                    Footer = viewModel.Footer,
+                    Signatories = (signatoriesIds != null)
+                        ? (await _unitOfWork.Personnel
+                        .FindAll<Personnel>(q => signatoriesIds.Contains(q.Id)))
+                        .ToList()
+                        : new List<Personnel>()
+                };
+
+                await _unitOfWork.ReportTemplates.Create(model);
+                await _unitOfWork.Save();
+
+                if (!await _unitOfWork.ReportTemplates.isExists(q => q.Id == model.Id))
+                    return ReturnResponse("Fail", ResponseStatus.Fail);
+
+                return ReturnResponse("Success!", ResponseStatus.Success);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return ReturnResponse("An error occured while saving the template", ResponseStatus.Fail);
+            }
+        }
+
+
 
 
         // -----------------------------------------------------------------------
@@ -43,10 +111,67 @@ namespace temsAPI.Controllers.ReportControllers
         /// </summary>
         /// <param name="viewModel"></param>
         /// <returns></returns>
-        //private async Task<string> ValidateTemplate(AddReportTemplateViewModel viewModel)
-        //{
-        //    // Invalid id provided (When it's the udpate case)
-        //    if(viewModel.Id != null && !await _unitOfWork.repo)
-        //}
+        private async Task<string> ValidateTemplate(AddReportTemplateViewModel viewModel)
+        {
+            // Invalid id provided (When it's the udpate case)
+            if (viewModel.Id != null && !await _unitOfWork.ReportTemplates
+                .isExists(q => q.Id == viewModel.Id))
+                return "Invalid id provided";
+
+            // Invalid subject
+            if (new List<string>() { "equipment", "rooms", "personnel", "allocations" }
+                .IndexOf(viewModel.Subject) == -1)
+                return "Invalid subject";
+
+            // Invalid types provided
+            if(viewModel.Types != null)
+                foreach (var item in viewModel.Types)
+                    if (!await _unitOfWork.EquipmentTypes.isExists(q => q.Id == item.Value))
+                        return $"{item.Label} is not a valid type";
+
+            // Invalid definitions provided
+            if(viewModel.Definitions != null)
+                foreach (var item in viewModel.Definitions)
+                    if (!await _unitOfWork.EquipmentDefinitions.isExists(q => q.Id == item.Value))
+                        return $"{item.Label} is not a valid definition";
+
+            // Invalid personnel provided
+            if (viewModel.Personnel != null)
+                foreach (var item in viewModel.Personnel)
+                    if (!await _unitOfWork.Personnel.isExists(q => q.Id == item.Value))
+                        return $"{item.Label} is not a valid personnel";
+
+            // Invalid rooms provided
+            if (viewModel.Rooms != null)
+                foreach (var item in viewModel.Rooms)
+                    if (!await _unitOfWork.Rooms.isExists(q => q.Id == item.Value))
+                        return $"{item.Label} is not a valid room";
+
+            // Invalid signatories provided
+            if (viewModel.Signatories != null)
+                foreach (var item in viewModel.Signatories)
+                    if (!await _unitOfWork.Personnel.isExists(q => q.Id == item.Value))
+                        return $"{item.Label} is not a valid type";
+
+            // Invalid SepparateBy
+            if (new List<string>() { "none", "room", "personnel", "type", "definition" }
+                .IndexOf(viewModel.SepparateBy) == -1)
+                return "Invalid SepparateBy";
+
+            // Invalid properties
+            List<string> commonTemsProps = new List<string>() { "temsid", "serialNumber", "price", "currency", "description", "identifier" };
+            if(viewModel.CommonProperties != null)
+                foreach (var item in viewModel.CommonProperties)
+                    if (!commonTemsProps.Contains(item) && !await _unitOfWork.Properties.isExists(q => q.Name == item))
+                        return $"{item} is not a valid property";
+
+            var specificProperties = viewModel.SpecificProperties?.SelectMany(q => q.Properties).ToList();
+            if(specificProperties != null)
+                foreach (var item in specificProperties)
+                    if (!await _unitOfWork.Properties.isExists(q => q.Name == item))
+                        return $"{item} is not a valid property";
+
+            return null;
+        }
     }
 }
--- API/temsAPI/temsAPI/ViewModels/Report/AddReportTemplateViewModel.cs
+++ API/temsAPI/temsAPI/ViewModels/Report/AddReportTemplateViewModel.cs
@@ -11,21 +11,21 @@ namespace temsAPI.ViewModels.Report
         public string Name { get; set; }
         public string Description { get; set; }
         public string Subject { get; set; }
-        public List<Option> Types { get; set; }
-        public List<Option> Definitions { get; set; }
-        public List<Option> Personnel { get; set; }
-        public List<Option> Rooms { get; set; }
+        public List<Option> Types { get; set; } = new List<Option>();
+        public List<Option> Definitions { get; set; } = new List<Option>();
+        public List<Option> Personnel { get; set; } = new List<Option>();
+        public List<Option> Rooms { get; set; } = new List<Option>();
         public string SepparateBy { get; set; }
-        public List<string> CommonProperties { get; set; }
-        public List<SpecificPropertyWrapper> SpecificProperties { get; set; }
+        public List<string> CommonProperties { get; set; } = new List<string>();
+        public List<SpecificPropertyWrapper> SpecificProperties { get; set; } = new List<SpecificPropertyWrapper>();
         public string Header { get; set; }
         public string Footer { get; set; }
-        public List<Option> Signatories { get; set; }
+        public List<Option> Signatories { get; set; } = new List<Option>();
     }
 
     public class SpecificPropertyWrapper
     {
         public string Type { get; set; }
-        public List<string> Properties { get; set; }
+        public List<string> Properties { get; set; } = new List<string>();
     }
 }

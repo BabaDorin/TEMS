173eaed8c7d356601bb457f74aa274076a79b704
Baba Dorin <bvd.dorin@gmail.com>
2021-03-31 14:52:56 +0300

    [fe-be-interaction] View personnel details from ag-grid button

--- UI/temsUI/src/app/tems-components/personnel/ag-grid-personnel/ag-grid-personnel.component.ts
+++ UI/temsUI/src/app/tems-components/personnel/ag-grid-personnel/ag-grid-personnel.component.ts
@@ -1,7 +1,11 @@
+import { PersonnelDetailsGeneralComponent } from './../personnel-details-general/personnel-details-general.component';
 import { TEMSComponent } from './../../../tems/tems.component';
 import { PersonnelService } from './../../../services/personnel-service/personnel.service';
 import { ViewPersonnelSimplified } from './../../../models/personnel/view-personnel-simplified.model';
 import { Component, OnInit } from '@angular/core';
+import { DialogService } from 'src/app/services/dialog-service/dialog.service';
+import { SnackService } from 'src/app/services/snack/snack.service';
+import { BtnCellRendererComponent } from 'src/app/public/ag-grid/btn-cell-renderer/btn-cell-renderer.component';
 
 @Component({
   selector: 'app-ag-grid-personnel',
@@ -17,19 +21,49 @@ export class AgGridPersonnelComponent extends TEMSComponent implements OnInit {
   private defaultColDef;
   private rowSelection;
   private rowData: [];
+  private frameworkComponents: any;
+  private pagination
+  private paginationPageSize;
+  loading: boolean = true;
 
   personnel: ViewPersonnelSimplified[];
 
   constructor(
-    private personnelService: PersonnelService
+    private personnelService: PersonnelService,
+    private dialogService: DialogService,
+    private snackService: SnackService
   ) { 
     super();
+
+    // enables pagination in the grid
+    this.pagination = true;
+
+    // sets 10 rows per page (default is 100)
+    this.paginationPageSize = 20;
+
+    this.frameworkComponents = {
+      btnCellRendererComponent: BtnCellRendererComponent
+    }
 
     this.columnDefs = [
       { field: 'name', sortable: true, filter: true, checkboxSelection: true, headerCheckboxSelection: true},
       { field: 'positions', sortable: true, filter: true },
       { field: 'allocatedEquipments', sortable: true, filter: true },
       { field: 'activeTickets', sortable: true, filter: true },
+      {
+        cellRenderer: 'btnCellRendererComponent',
+        cellRendererParams: {
+          onClick: this.details.bind(this),
+          label: 'Details'
+        }
+      },
+      {
+        cellRenderer: 'btnCellRendererComponent',
+        cellRendererParams: {
+          onClick: this.archieve.bind(this),
+          label: 'Archieve'
+        }
+      }
     ];
 
     this.defaultColDef = {
@@ -54,6 +88,7 @@ export class AgGridPersonnelComponent extends TEMSComponent implements OnInit {
       .subscribe(result => {
         console.log(result);
         this.rowData = result;
+        this.loading = false;
       }));
   }
 
@@ -65,5 +100,29 @@ export class AgGridPersonnelComponent extends TEMSComponent implements OnInit {
 
   getSelectedNodes(){
     return this.gridApi.getSelectedNodes().map(q => q.data);
+  }
+
+  details(e) {
+    this.dialogService.openDialog(
+      PersonnelDetailsGeneralComponent,
+      [
+        { label: "displayViewMore", value: true },
+        { label: "personnelId", value: e.rowData.id },
+      ]
+    )
+  }
+
+  archieve(e){
+    // if(!confirm("Are you sure you want to archive this item? It will result in archieving all of it's logs and allocations"))
+    // return;
+
+    // this.subscriptions.push(
+    //   this.personnelService.archievePersonnel(e.rowData.id)
+    //   .subscribe(result => {
+    //     if(this.snackService.snack(result)){
+    //       this.gridApi.applyTransaction({ remove: [e.rowData] });
+    //     }
+    //   })
+    // )
   }
 }
--- UI/temsUI/src/app/tems-components/personnel/personnel-details-general/personnel-details-general.component.ts
+++ UI/temsUI/src/app/tems-components/personnel/personnel-details-general/personnel-details-general.component.ts
@@ -1,38 +1,57 @@
+import { PersonnelService } from './../../../services/personnel-service/personnel.service';
 import { TEMSComponent } from './../../../tems/tems.component';
-import { PersonnelService } from 'src/app/services/personnel-service/personnel.service';
 import { ViewPersonnel } from './../../../models/personnel/view-personnel.model';
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Property } from 'src/app/models/equipment/view-property.model';
+import { Router } from '@angular/router';
+import { SnackService } from 'src/app/services/snack/snack.service';
 
 @Component({
   selector: 'app-personnel-details-general',
   templateUrl: './personnel-details-general.component.html',
   styleUrls: ['./personnel-details-general.component.scss']
 })
-export class PersonnelDetailsGeneralComponent implements OnInit, OnChanges {
+export class PersonnelDetailsGeneralComponent extends TEMSComponent implements OnInit {
 
+  @Input() personnelId: string;
   @Input() personnel: ViewPersonnel;
+  @Input() displayViewMore: boolean = false;
+  dialogRef;
 
   personnelProperties: Property[];
-  edit: boolean = false;
-
-  constructor() { 
+  constructor(
+    private personnelService: PersonnelService,
+    private route: Router,
+    private snackService: SnackService
+  ) {
+    super();
   }
 
   ngOnInit(): void {
-    
+    this.subscriptions.push(
+      this.personnelService.getPersonnelById(this.personnelId)
+      .subscribe(response => {
+        console.log(response);
+        this.personnel = response;
+
+        this.personnelProperties = [
+          { displayName: 'Name', value: this.personnel.name },
+          { displayName: 'Position', value: "display them in a fancy way" },
+          { displayName: 'Phone Number', value: this.personnel.phoneNumber },
+          { displayName: 'Email', value: this.personnel.email },
+          { displayName: 'Active equipment allocations', value: this.personnel.allocatedEquipments },
+          { displayName: 'Active tickets', value: this.personnel.activeTickets },
+          { displayName: 'Room supervisories', value: "Display them in a fancy way" },
+        ];
+      })
+    )
   }
 
-  ngOnChanges(): void {
-    if(this.personnel != undefined)
-      this.personnelProperties = [
-        { displayName: 'Name', value: this.personnel.name },
-        { displayName: 'Position', value: "display them in a fancy way" },
-        { displayName: 'Phone Number', value: this.personnel.phoneNumber },
-        { displayName: 'Email', value: this.personnel.email },
-        { displayName: 'Active equipment allocations', value: this.personnel.allocatedEquipments },
-        { displayName: 'Active tickets', value: this.personnel.activeTickets },
-        { displayName: 'Room supervisories', value: "Display them in a fancy way" },
-      ]
+  viewMore(){
+    console.log('here');
+    this.route.navigateByUrl('/personnel/details/' + this.personnelId);
+    
+    if(this.dialogRef != undefined)
+      this.dialogRef.close();
   }
 }

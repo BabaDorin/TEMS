96e09a3d39bdea5a735665093a33625631381e9f
BabaDorin <bvd.dorin@gmail.com>
2021-06-28 19:41:53 +0300

    [cleaning] Remove useless components and files

--- UI/temsUI/src/app/models/ag-grid/ag-grid-helper.model.ts
+++
@@ -1,7 +0,0 @@
-export class AgGridHelper{
-    static isFirstColumn(params) {
-        var displayedColumns = params.columnApi.getAllDisplayedColumns();
-        var thisIsFirstColumn = displayedColumns[0] === params.column;
-        return thisIsFirstColumn;
-    }
-}
